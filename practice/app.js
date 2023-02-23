9451caf8643d148b5b459d13ad3264c3cb83af95
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-02-23 15:15:37 +0500

    added files

---
+++ practice/app.js
@@ -0,0 +1,116 @@
+
+
+// var num = [12,32,34,11,43,55]
+// console.log(num);
+// function maximum() {
+// var num = [12,32,34,11,43,55]
+// var max = num[0];
+// for(i=0; i<num.length; i++){
+//     if(num[i]>max) {
+//     max = num[i];
+     
+// }
+// }
+// document.write("The max num is :" , + max + "<br/>");
+// console.log(max);
+// }
+// maximum();
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// var arr = [12,34,64,75,23,5]
+// console.log(arr);
+// function minimum() {
+//     var arr = [12,34,64,75,23,5]
+//     var min = arr[0];
+//     for(i=0; i<arr.length; i++){
+//         if(arr[i]<min){
+//             min = arr[i];
+//         }
+//     }
+// document.write("The minimum number is :" + min);
+// console.log(min);
+// }
+// minimum();
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// var num = [123,543,234,241,7,345,521]
+// console.log(num);
+// function minimum() {
+//     var num = [123,543,234,241,7,345,521]
+//     var min = num[0];
+//     for(i=0; i<num.length; i++){
+//         if(num[i]<min){
+//             min = num[i]; 
+//         }
+//     }
+//     document.write("The minimum number is :" + min);
+//     console.log(min);
+// }
+// minimum();
+
+//<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// var num = [123,543,234,241,7,345,521]
+// console.log(num);
+// function maximum() {
+//     var num = [123,543,234,241,7,345,521]
+//     var max = num[0];
+//     for(i=0; i<num.length; i++){
+//         if(num[i]>max){
+//             max = num[i]; 
+//         }
+//     }
+//     document.write("The maximum number is :" + max);
+//     console.log(max);
+// }
+// maximum();
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+var num = [1,2,3,4,5,7,8,9,10]
+console.log(num);
+var missingNumber;
+for(i=1; i<=num.length; i++){
+    if(num !== i+1){
+    missingNumber = num;
+    console.log(num);
+     break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
